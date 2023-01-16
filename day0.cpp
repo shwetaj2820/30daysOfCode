f6fee0da9f3c42fddab46cdd32c6a1e291b3849d
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2023-01-16 19:24:09 +0530

    Run successfully day0.cpp

---
+++ day0.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    string inputString;
+    getline(cin,inputString);
+    cout<<"Hello, World.\n"<<inputString;
+    return 0;
+}
